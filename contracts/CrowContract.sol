5f03748f979481f61be646b17f891d977ce86ec7
juanigna <jibocchi14@gmail.com>
2022-11-17 14:49:51 -0300

    First part of testing

--- contracts/CrowContract.sol
+++ contracts/CrowContract.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-interface IERC20 {
-    function transfer(address recipient, uint amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-}
+import "./IERC20.sol";
+
 
 /*
 
--- test/CrowContract.js
+++ test/CrowContract.js
@@ -5,15 +5,28 @@ let signers = {};
 
 let ContractFactory;
 let ContractInstance;
-
+let paymentTokenFactory;
+let PaymentTokenInstance;
 
 describe("CrowFunding", function(){
     describe("Deploy", function(){
         it("Shoul deploy the smart contract", async function(){
             const [deployer, firstUser] = await ethers.getSigners();
+            signers.deployer =deployer;
+            signers.firstUser = firstUser;
+            paymentTokenFactory = await ethers.getContractFactory("ERC20Basic");
+            PaymentTokenInstance = await paymentTokenFactory.deploy();
             ContractFactory = await ethers.getContractFactory("CrowdFund");
-            ContractInstance = await ContractFactory.deploy()
+            ContractInstance = await ContractFactory.deploy(PaymentTokenInstance.address);
+        })
+    })
+    
 
+    describe("Adding funds to an user", function(){
+        it("Should allow to mint token to an user", async function(){
+            let PaymentTokenInstanceSeconUser = await PaymentTokenInstance.connect(signers.firstUser);
+            let mintTokenTx = await PaymentTokenInstanceSeconUser.mint(100);
+            await mintTokenTx.wait();
         })
     })
 })
